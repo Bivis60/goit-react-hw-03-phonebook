bf680bb3a928d16030a81f36ae63b544825945d4
Bivis60 <89808390+Bivis60@users.noreply.github.com>
2023-04-25 22:52:44 +0300

    Кінець проекту

--- src/components/ContactForm/ContactForm.styled.js
+++ src/components/ContactForm/ContactForm.styled.js
@@ -21,4 +21,8 @@ export const Button = styled.button`
   border-radius: 4px;
   box-shadow: rgb(0 0 0 / 20%) 0px 3px 3px -2px,
     rgb(0 0 0 / 14%) 0px 3px 4px 0px, rgb(0 0 0 / 12%) 0px 1px 8px 0px;
+  cursor: pointer;
+  :hover {
+    background-color: lightpink;
+  }
 `;
--- src/components/ContactList/ContactList.styled.js
+++ src/components/ContactList/ContactList.styled.js
@@ -4,6 +4,7 @@ export const Li = styled.li`
   display: flex;
   justify-content: space-between;
   margin-bottom: 8px;
+  gap: 8px;
 `;
 
 export const Span = styled.span`
@@ -20,4 +21,8 @@ export const Button = styled.button`
   margin-left: 24px;
   box-shadow: rgb(0 0 0 / 20%) 0px 3px 3px -2px,
     rgb(0 0 0 / 14%) 0px 3px 4px 0px, rgb(0 0 0 / 12%) 0px 1px 8px 0px;
+  cursor: pointer;
+  :hover {
+    background-color: lightpink;
+  }
 `;
